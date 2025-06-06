6fe617bfd1417b70c627e409b973179d2b4a40b0
José Carlos Chávez <jcchavezs@gmail.com>
2025-06-06 10:23:43 +0200

    feat: adds support for subdir.
    This allows to create new execers for a subdir inheriting the properties from the parent. This is useful when you are willing to run functions in multimodules e.g. go modules or npm projects keeping the simplicity of exec.

--- exec/exec_test.go
+++ exec/exec_test.go
@@ -2,6 +2,7 @@ package exec
 
 import (
 	"context"
+	"path/filepath"
 	"testing"
 
 	"github.com/spf13/afero"
@@ -14,7 +15,7 @@ func TestFS(t *testing.T) {
 	_, err := e.RunX(context.Background(), "touch", "a.txt")
 	require.NoError(t, err)
 
-	fs := e.FS()
+	fs := FS(e)
 
 	t.Run("exists", func(t *testing.T) {
 		exists, err := afero.Exists(fs, "a.txt")
@@ -27,4 +28,30 @@ func TestFS(t *testing.T) {
 		require.False(t, exists)
 		require.NoError(t, err)
 	})
+}
+
+func TestTrimStdout(t *testing.T) {
+	dir := t.TempDir()
+	e := NewExecer(dir, false)
+	out, err := e.RunX(context.Background(), "echo", "Hello World")
+	require.NoError(t, err)
+	require.Equal(t, "Hello World\n", out)
+
+	out, err = TrimStdout(e.RunX(context.Background(), "echo", "Hello World"))
+	require.NoError(t, err)
+	require.Equal(t, "Hello World", out)
+}
+
+func TestSub(t *testing.T) {
+	dir := t.TempDir()
+	e := NewExecer(dir, false)
+	_, err := Sub(e, "subdir")
+	require.Error(t, err)
+
+	_, err = e.RunX(context.Background(), "mkdir", "subdir")
+	require.NoError(t, err)
+
+	se, err := Sub(e, "subdir")
+	require.NoError(t, err)
+	require.Equal(t, filepath.Join(dir, "subdir"), se.dir)
 }
