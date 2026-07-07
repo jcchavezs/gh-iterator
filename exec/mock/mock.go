effab3ae7451ee66e11127426416a24187c62cc9
José Carlos Chávez <jcchavezs@gmail.com>
2026-07-08 00:19:10 +0200

    Revert "feat: adds CallIs function to write better tests when using xr mocks …" (#43)
    This reverts commit 3a40abb3bcca4aa67addbbd5caa59c7bb22489b2.

--- exec/mock/mock.go
+++ exec/mock/mock.go
@@ -17,11 +17,9 @@ type Execer struct {
 	RunWithStdinXFn func(ctx context.Context, stdin io.Reader, command string, args ...string) (string, error)
 	Logger          *slog.Logger
 
-	// 'self' allows you to return the same mock Execer when calling WithEnv, WithLogFields or Sub
-	// This is useful when you want to chain calls and keep the same mock behavior.
-	WithEnvFn       func(self Execer, kv ...string) iteratorexec.Execer
-	WithLogFieldsFn func(self Execer, fields ...any) iteratorexec.Execer
-	SubFn           func(self Execer, subpath string) (iteratorexec.Execer, error)
+	WithEnvFn       func(kv ...string) iteratorexec.Execer
+	WithLogFieldsFn func(fields ...any) iteratorexec.Execer
+	SubFn           func(subpath string) (iteratorexec.Execer, error)
 	GenerateFSFn    func() afero.Fs
 }
 
@@ -52,15 +50,15 @@ func (x Execer) Log(ctx context.Context, level slog.Level, msg string, fields ..
 func (x Execer) DebugShell(context.Context) {}
 
 func (x Execer) WithEnv(kv ...string) iteratorexec.Execer {
-	return x.WithEnvFn(x, kv...)
+	return x.WithEnvFn(kv...)
 }
 
 func (x Execer) WithLogFields(fields ...any) iteratorexec.Execer {
-	return x.WithLogFieldsFn(x, fields...)
+	return x.WithLogFieldsFn(fields...)
 }
 
 func (x Execer) Sub(subpath string) (iteratorexec.Execer, error) {
-	return x.SubFn(x, subpath)
+	return x.SubFn(subpath)
 }
 
 func (x Execer) GenerateFS() afero.Fs {
